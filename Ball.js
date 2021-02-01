824501ef0300e7a1f93c1572896b88bb7b76d5b6
ROHITSHARMA-coder <74244439+ROHITSHARMA-coder@users.noreply.github.com>
2021-02-01 16:27:07 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,43 @@
+class Ball{
+    constructor(x,y,radius){
+        var options={
+            
+            density: 1.0,
+            frictionAir:0.005
+        }
+        this.radius=radius;
+        
+        this.body=Bodies.circle(x,y,radius,options);
+        World.add(world,this.body);
+    }
+    display(){
+        var pos= this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(this.body.angle)
+        ellipseMode(RADIUS);
+        stroke("cyan");
+        fill("red")
+ellipse(0,0,this.radius,this.radius);
+pop();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
